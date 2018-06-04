afd13334bf0a334ee2fcdb4c3403289b9ab2bae6
Stanton Cady <stantontcady@gmail.com>
2018-06-03 21:02:50 -0400

    Added simple script to convert csv log from prototype to gpx.

---
+++ csv_log_to_gpx.py
@@ -0,0 +1,28 @@
+from dateutil import parser
+from gpxpy.gpx import GPX, GPXTrack, GPXTrackSegment, GPXTrackPoint
+from pandas import read_csv
+
+basename = '2018-05-25'
+
+df = read_csv('{0}.csv'.format(basename))
+
+gpx_obj = GPX()
+
+gpx_track = GPXTrack()
+gpx_obj.tracks.append(gpx_track)
+
+gpx_segment = GPXTrackSegment()
+gpx_track.segments.append(gpx_segment)
+
+gpx_segment.points.extend(
+    (
+        GPXTrackPoint(
+            time=parser.parse(row.timestamp),
+            latitude=row.latitude,
+            longitude=row.longitude
+        ) for row in df.itertuples()
+    )
+)
+
+with open('{0}.gpx'.format(basename), 'w') as gpx_file:
+    gpx_file.write(gpx_obj.to_xml())
